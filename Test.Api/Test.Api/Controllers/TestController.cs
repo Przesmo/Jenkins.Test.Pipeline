bb3e6acc3b8e3c7e879b70810631c27a2ebed27c
przesmo <przesmolana@gmail.com>
2019-11-14 19:44:54 +0000

    Added test project

---
+++ Test.Api/Test.Api/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Test.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Test()
+        {
+            return "Pass";
+        }
+    }
+}
